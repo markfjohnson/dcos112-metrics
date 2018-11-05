2a7ceb0ae6f0b6a0ab51ec62199c9d275951954d
markjohnson <markfjohnson@gmail.com>
2018-11-05 15:19:47 -0500

    Initial commit

---
+++ Installation/install_prometheus.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+dcos package install --yes marathon-lb
+dcos marathon app add prometheus-marathonlb.json
+dcos package install --yes prometheus
+dcos package install --yes grafana
